1c9864dbbd0eb373e7dfc1f47365c7980761f3b9
natandreli <natalia.garcia9@udea.edu.co>
2026-05-05 10:18:58 -0500

    refactor: :recycle: rename optimize to optimization

--- src/components/controllers/app-routes.tsx
+++ src/components/controllers/app-routes.tsx
@@ -3,7 +3,7 @@ import { AppLayout } from '@/layout/app-layout'
 import { HomePage } from '@/pages/home'
 import { ModelsPage } from '@/pages/models'
 import { SimulationPage } from '@/pages/simulation'
-import { OptimizePage } from '@/pages/optimize'
+import { OptimizationPage } from '@/pages/optimization'
 
 export const AppRoutes = () => {
   return (
@@ -12,7 +12,7 @@ export const AppRoutes = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/models" element={<ModelsPage />} />
         <Route path="/simulation" element={<SimulationPage />} />
-        <Route path="/optimize" element={<OptimizePage />} />
+        <Route path="/optimization" element={<OptimizationPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
