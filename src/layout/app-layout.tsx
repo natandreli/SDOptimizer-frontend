58b8da6654845dc9bec8f29e719ad6b0ee6a4e7b
natandreli <natalia.garcia9@udea.edu.co>
2026-05-24 15:34:44 -0500

    style: :lipstick: update max-width of main container to 1600px

--- src/layout/app-layout.tsx
+++ src/layout/app-layout.tsx
@@ -9,7 +9,7 @@ export const AppLayout = () => {
     <div className="relative flex h-[100dvh] flex-col overflow-hidden bg-[var(--color-background)]/80">
       <AnimatedBackground />
       <Header />
-      <main className="relative z-10 container mx-auto flex min-h-0 w-full max-w-7xl flex-1 flex-col px-5 py-6 sm:py-8 lg:px-10">
+      <main className="relative z-10 container mx-auto flex min-h-0 w-full max-w-[1600px] flex-1 flex-col px-5 py-6 sm:py-8 lg:px-10">
         <Outlet />
       </main>
       <Footer />
